29eebd7e188129ba96993a46c510fbcf7337a293
Michal Lehuta <66588074+MichalLeh@users.noreply.github.com>
2023-03-01 10:40:31 +0100

    Update DecryptUnicodeAlgorithm.java
    Add comment

--- src/encryptdecrypt/DecryptUnicodeAlgorithm.java
+++ src/encryptdecrypt/DecryptUnicodeAlgorithm.java
@@ -1,6 +1,13 @@
 package encryptdecrypt;
 
-public class DecryptUnicodeAlgorithm implements Algorithm{
+public class DecryptUnicodeAlgorithm implements Algorithm {
+    /**
+     * Decode the current char by shifting it by the key using unicode table
+     *
+     * @param data The original message to be decoded.
+     * @param key  The key to decode the message.
+     * @return The decoded message as a String.
+     */
     public String execute(String data, int key) {
         StringBuilder resultCipher = new StringBuilder();
 
