28df1e91bc48d26995fb85df81df892e2eb91bb7
Michal Lehuta <66588074+MichalLeh@users.noreply.github.com>
2023-03-01 10:41:02 +0100

    Update EncryptShiftAlgorithm.java
    Add comment

--- src/encryptdecrypt/EncryptShiftAlgorithm.java
+++ src/encryptdecrypt/EncryptShiftAlgorithm.java
@@ -1,6 +1,14 @@
 package encryptdecrypt;
 
-public class EncryptShiftAlgorithm implements Algorithm{
+public class EncryptShiftAlgorithm implements Algorithm {
+       /**
+     * Encode the current char by shifting each letter by the key number
+     * from "a" to "z" and from "A" to "Z".
+     *
+     * @param data Data to be encoded
+     * @param key  Key to encode the message
+     * @return The encoded data as a String
+     */
     public String execute(String data, int key) {
         StringBuilder resultCipher = new StringBuilder();
         char startLetter;
